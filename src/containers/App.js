3398ed38d17ee24cc5278a66b6fb855ccc9f65c9
Vishal <vishal@appcues.com>
2019-10-16 14:39:49 -0400

    clean up intial commit

---
+++ src/containers/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "../styles/App.css";
+import MainContainer from "./MainContainer";
+
+class App extends Component {
+  render() {
+    return <MainContainer />;
+  }
+}
+
+export default App;
---
+++ src/containers/MainContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../styles/App.css";
+
+class MainContainer extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default MainContainer;
